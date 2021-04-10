49501a8b2b7ac814731d687f066acec4d03f79fa
wangyifei <995340474@qq.com>
2021-04-10 19:11:50 +0800

    翻译功能实现

--- chatApp/src/main/java/cn/edu/xmu/config/RestTemplateConfig.java
+++ chatApp/src/main/java/cn/edu/xmu/config/RestTemplateConfig.java
@@ -1,14 +1,18 @@
 package cn.edu.xmu.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
+
     @Bean
-    public RestTemplate restTemplate() {
-        // 我不熟悉RestTemplate的配置，就这样简单的搞一下好了。
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        RestTemplate restTemplate = builder.build();
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+        return restTemplate;
     }
 }
